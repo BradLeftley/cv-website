9113c6df68cd5dd388b9ee81ba30436644359a4b
BradLeftley <b_leftley56@yahoo.co.uk>
2020-05-08 12:10:36 +0100

    Observe property

--- app/components/nav-link.js
+++ app/components/nav-link.js
@@ -1,11 +1,5 @@
-/* eslint-disable no-debugger */
 import Component from '@ember/component';
 import { inject as service } from "@ember/service";
-import { computed, observer } from "@ember/object";
-import { on } from '@ember/object/evented';
-import { run } from '@ember/runloop';
-import { addObserver } from '@ember/object/observers';
-import EmberObject from '@ember/object';
 
 export default Component.extend({
     viewportHandler: service('viewport-handler'),
@@ -15,11 +9,15 @@ export default Component.extend({
     
     init(){
         this._super(...arguments);
-        let linkId = this.linkId;
+        const linkId = this.linkId;
         this.set(`viewportHandler.${linkId}`, false);
+        this.set('viewportHandler.current', null);
 
-        this.addObserver(`viewportHandler.${linkId}`,function() {
-            this.set('active', this.get(`viewportHandler.${linkId}`));
+        //Check if in viewport then set 
+        this.set('active', this.viewportHandler.current === linkId);
+
+        this.addObserver(`viewportHandler.current`,function() {
+            this.set('active', this.viewportHandler.current === linkId);
         });
       },
       willDestroy() {
--- app/services/viewport-handler.js
+++ app/services/viewport-handler.js
@@ -1,36 +1,18 @@
-/* eslint-disable no-debugger */
 import Service from '@ember/service';
-import { get, set, setProperties, computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 
 export default Service.extend({
     store: service(),
-    currentId: null,
-    about: false,
-    experience: false,
-    education: false,
-    opensource: false,
-    portfolio: false,
-    design: false,
-    contact: false,
-
-    resetViewPort() {
-        setProperties(this, {
-          about: false,
-          experience: false,
-          education: false,
-          opensource: false,
-          portfolio: false,
-          design: false,
-          contact: false
-        });
-      },
-    
-      updateViewPortActivity(status, areaName) {
-        if (status === true) {
-          this.resetViewPort();
-          set(this, areaName, status);
-        }
-      }
+    current: null,
+    init() {
+      this._super(...arguments);
+      this.set('current', null);
+    },
+   
+    updateViewPortActivity(status, areaName) {
+      if(status === true) {
+         this.set('current', areaName);
+       } 
+     }
 });
