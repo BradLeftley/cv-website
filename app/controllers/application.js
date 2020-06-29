861b43675fcb5b0508c3e056abf6a6756296001a
BradLeftley <b_leftley56@yahoo.co.uk>
2020-06-29 21:54:54 +0100

    Update package version

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,5 @@
 import Controller from '@ember/controller';
 import { inject as service } from "@ember/service";
 export default Controller.extend({
-    viewportHandler: service('viewport-handler'),
+    // viewportHandler: service('viewport-handler'),
 });
